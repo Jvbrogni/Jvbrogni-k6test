fa80a2af33a5db8935f2345bbb901cdd83865cbd
Joao Vitor Brogni Vamerlati <joao.150419@alunosatc.edu.br>
2025-06-17 19:54:53 -0300

    first commit

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,46 +4,51 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+// Métricas personalizadas
+export const getUsersDuration = new Trend('get_users_duration', true);
+export const statusCodeOK = new Rate('status_code_ok');
 
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '10s', target: 10 },
+    { duration: '25s', target: 35 },
+    { duration: '25s', target: 55 },
+    { duration: '25s', target: 55 },
+    { duration: '25s', target: 70 },
+    { duration: '25s', target: 90 },
+    { duration: '25s', target: 100 },
+    { duration: '25s', target: 150 },
+    { duration: '25s', target: 200 },
+    { duration: '25s', target: 240 },
+    { duration: '25s', target: 255 },
+    { duration: '40s', target: 300 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<5700'],   
+    http_req_failed: ['rate<0.12'],      
+  },
 };
 
 export function handleSummary(data) {
   return {
     './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
   };
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const url = 'https://reqres.in/api/users?page=2';
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  const res = http.get(url, {
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-  const OK = 200;
+  getUsersDuration.add(res.timings.duration);
 
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
+  const ok = res.status === 200;
+  statusCodeOK.add(ok);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET /api/users?page=2 – Status 200': () => ok,
   });
 }
